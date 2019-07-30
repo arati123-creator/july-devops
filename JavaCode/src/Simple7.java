b3f5cabb80798166f5a27d3adccd1514310d8efb
Arati Pawashe <aratipawashe@Aratis-MacBook-Pro.local>
2019-07-29 20:21:23 -0700

    .

---
+++ JavaCode/src/Simple7.java
@@ -0,0 +1,14 @@
+
+public class Simple7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	        int a=500;
+			byte b= 30;  
+			short s= 10; 
+			long l= (70000+15*a) + s +b;				
+			System.out.println(l);
+			}
+	}
+
+
